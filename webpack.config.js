958c40487595c2ea470788ef90fff004417d27d9
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-10-25 16:28:06 -0300

    Webpack dev serve e Source Map configurados

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 
 module.exports = {
+    mode:"development",
+    devtool:"eval-source-map",
+    
     entry: path.resolve(__dirname, 'src', "index.jsx"),
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js"
@@ -13,6 +17,17 @@ module.exports = {
     resolve:{
         extensions:[".js", ".jsx"]
     },
+
+    devServer: {
+        static: path.resolve(__dirname, 'public')
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', "index.html"),
+        })
+    ],
+    
+
     module:{
         rules:[
             {
